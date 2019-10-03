55a3e0e9b60bfcd5740291d6157d34161e110ea2
Danit Lentz <danit@Danits-MBP.hsd1.nj.comcast.net>
2019-10-03 10:22:06 -0400

    handlebars connected for main page

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-const express = require('express')
-const app = express()
-require('handlebars');
+const express = require('express');
+const app = express();
+var exphbs = require("express-handlebars");
+
+// Handlebars
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+})
+);
+app.set("view engine", "handlebars");
+
+// routing
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log('server is starting at port ', 8080)
+});
